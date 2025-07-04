8789bda825574c9b02c5a79c9b19f6f7b849fbd1
thetanish1 <tanishdewase25@gmail.com>
2025-07-04 11:06:46 +0530

    added the include tag in course.html file

--- courseapp/courseapp/admin.py
+++ courseapp/courseapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Contact,Certificate,Services
+from .models import Contact,Certificate
 
 admin.site.register(Contact)
 admin.site.register(Certificate)
-admin.site.register(Services)
+
